ab46337eed104a03bacfaedd1dc6bdcfd0be17b4
mkoertgen <marcel.koertgen@gmail.com>
2017-10-04 14:34:23 +0200

    Allow self-signed certs. Fixes #3

--- options.js
+++ options.js
@@ -2,7 +2,10 @@ module.exports = {
   appUrl: process.env.APP_URL || 'http://localhost:3000',
   screenshotPath: process.env.SCREENSHOTS_PATH || './screenshots',
   puppeteer: {
+    // cf.: https://github.com/GoogleChrome/puppeteer/blob/master/docs/api.md#puppeteerlaunchoptions
+    ignoreHTTPSErrors: true,
     // headless: false,
+    // cf.: https://peter.sh/experiments/chromium-command-line-switches/
     args: [
       // for docker, cf.:
       '--no-sandbox',
